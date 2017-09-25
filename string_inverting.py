6fc64197d49f9b984fd05d6eaec0bbca81d454f8
EddyCodeIt <g00315079@gmit.ie>
2017-09-25 15:20:01 +0100

    10. Inverting String in Python. Slice notation.

---
+++ string_inverting.py
@@ -0,0 +1,11 @@
+
+word = input()
+
+def reverse_string():
+    return word[::-1]
+
+# [::-1] inverts string 
+print(reverse_string())
+
+# about slice notation in python https://stackoverflow.com/questions/509211/explain-slice-notation
+# git commit -m "10. Inverting String in Python. Slice notation."
